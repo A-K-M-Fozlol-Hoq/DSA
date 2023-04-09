9e7ce45cf9fe9d85ec7d413399cb2ff11988f43c
A.K.M Fozlol Hoq <akmfozlolhoq@gmail.com>
2023-04-09 14:00:07 +0600

    added sum of 2 numbers

---
+++ Code/programiz/1. dsa introduction/0. WhatIsAnAlgorithm/add_two_numbers_enter_by_user.py
@@ -0,0 +1,14 @@
+'''
+Algorithm
+step 1: Start
+setp 2: Declare 3 variables named num1, num2 and sum
+step 3: Read values of num1 and num2 from user
+step 4: Add num1 and num2 and assign to sum variable (sum = num1 + num2)
+setp 5: Show output
+step 6: Stop
+'''
+num1, num2, sum = None, None, None
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number:"))
+sum = num1+ num2
+print(f"sum of {num1} and {num2} is {sum}")
